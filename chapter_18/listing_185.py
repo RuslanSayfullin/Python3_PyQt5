2c30424fd0e332b64c417f88aa88b8e7d94bfa70
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-05-06 15:37:51 +0500

    Управление прозрачностью окна

---
+++ chapter_18/listing_185.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from PyQt5 import QtWidgets
+import sys
+
+# Полупрозрачное окно
+app = QtWidgets.QApplication(sys.argv)
+window = QtWidgets.QWidget()
+window.setWindowTitle("Полупрозрачное окно")
+window.resize(300, 100)
+window.setWindowOpacity(0.9)
+window.show()
+print(window.windowOpacity()) # Выведет: 0.4980392156862745
+sys.exit(app.exec_())
