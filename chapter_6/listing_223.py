060f0633dc899ba387bf89c57d543790fff3784c
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-07-25 00:03:29 +0500

    Простой список QListView

---
+++ chapter_6/listing_223.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from PyQt5 import QtGui, QtWidgets
+import sys
+
+# Простой список QListView
+lv = QtWidgets.QListView()
+sti = QtGui.QStandardItemModel(parent = window)
+lst = ['Perl', 'PHP', 'Python', 'Ruby']
+for row in range(0, 4):
+    if row == 2:
+        iconfile = 'python.png'
+    else:
+        iconfile = 'icon.png'
+    item = QtGui.QStandardItem(QtGui.QIcon(iconfile), lst[row])
+    sti.appendRow(item)
+lv.setModel(sti)
