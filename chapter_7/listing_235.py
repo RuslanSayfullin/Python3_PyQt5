ba865e9ea12989b65c538aedae8e2150cb214b37
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2024-10-07 15:23:07 +0500

    Выполнение SQL-запросов

---
+++ chapter_7/listing_235.py
@@ -0,0 +1,14 @@
+# Использование метода bindValue() для задания параметров по их символьным обозначениям
+from PyQt5 import QtWidgets, QtSql
+import sys
+
+app = QtWidgets.QApplication(sys.argv)
+con = QtSql.QSqlDatabase.addDatabase('QSQLITE')
+con.setDatabaseName('data.sqlite')
+con.open()
+query = QtSql.QSqlQuery()
+query.prepare("insert into good values(null, :name, :count)")
+query.bindValue(':name', 'Флеш-накопитель')
+query.bindValue(':count', 20)
+query.exec_()
+con.close()
